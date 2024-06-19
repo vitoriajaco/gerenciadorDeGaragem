2a1aa794a0c0cd551265e182f8412b15eefa0070
vitoriajaco <vitoriajaco@gmail.com>
2024-06-18 21:05:20 -0300

    add: enum marcas de automovel

---
+++ src/main/java/gerenciadorDeGaragem/enums/Marca.java
@@ -0,0 +1,23 @@
+package gerenciadorDeGaragem.enums;
+
+public enum Marca {
+
+    FIAT,
+    CHEVROLET,
+    FORD,
+    TOYOTA,
+    HONDA,
+    BYD,
+    RENAULT,
+    NISSAN,
+    VOLKSWAGEN,
+    JEEP,
+    PEUGEOT,
+    YAMAHA,
+    MOTTU,
+    SHINERAY,
+    BMW,
+    SUZUKI
+
+    
+}
