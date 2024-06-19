2923e4350451fd7712cf57212625170ad3ef16b3
vitoriajaco <vitoriajaco@gmail.com>
2024-06-19 12:08:16 -0300

    add: classes repository

---
+++ src/main/java/gerenciadorDeGaragem/repository/ProprietarioRepository.java
@@ -0,0 +1,11 @@
+package gerenciadorDeGaragem.repository;
+
+import gerenciadorDeGaragem.model.Proprietario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProprietarioRepository extends JpaRepository<Proprietario, Long> {
+
+
+}
---
+++ src/main/java/gerenciadorDeGaragem/repository/VeiculoRepository.java
@@ -0,0 +1,9 @@
+package gerenciadorDeGaragem.repository;
+
+import gerenciadorDeGaragem.model.Veiculo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VeiculoRepository extends JpaRepository<Veiculo, Long> {
+}
